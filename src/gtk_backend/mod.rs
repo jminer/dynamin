719a2e2b9d44d29a29aede2ed238d06d907a46b0
Jordan Miner <jminer7@gmail.com>
2021-08-19 01:31:38 -0500

    Do some work on the GTK backend

--- src/gtk_backend/mod.rs
+++ src/gtk_backend/mod.rs
@@ -1,6 +1,22 @@
 
+use std::os::raw::c_char;
+
+use smallvec::SmallVec;
+
 pub use self::window_backend::WindowBackend;
 
 pub mod window_backend;
 
-
+pub fn str_to_c_vec<'a: 'b, 'b, A: ::smallvec::Array<Item=u8>>(s: &'a str, buf: &'b mut SmallVec<A>) -> *const c_char {
+    // `CString` in the std library doesn't check if the &str already ends in a null terminator
+    // It allocates and pushes a 0 unconditionally. However, I can add the null to string literals
+    // and avoid many allocations.
+    if s.as_bytes().last() == Some(&0) && !s.as_bytes()[..s.len() - 1].contains(&b'\0') {
+        s.as_bytes().as_ptr() as *const c_char
+    } else {
+        buf.grow(s.len() + 1);
+        buf.extend(s.as_bytes().iter().map(|c| if *c == b'\0' { b'?' } else { *c }));
+        buf.push(0);
+        (&buf[..]).as_ptr() as *const c_char
+    }
+}
--- src/gtk_backend/window_backend.rs
+++ src/gtk_backend/window_backend.rs
@@ -1,16 +1,111 @@
 
-use super::super::backend::GenericWindowBackend;
+use std::cell::Cell;
+use std::ptr;
+use std::rc::Weak;
 
+use glib_sys::gboolean;
+use gtk_sys::{GTK_WINDOW_TOPLEVEL, GtkWidget, GtkWindow, gtk_widget_destroy, gtk_widget_hide, gtk_widget_show, gtk_window_new, gtk_window_set_resizable, gtk_window_set_title};
+use smallvec::SmallVec;
 
-type HANDLE = uint;
+use crate::{Visibility, WindowData};
+use crate::generic_backend::GenericWindowBackend;
+
+use super::str_to_c_vec;
 
 pub struct WindowBackend {
-    tmp: int
+    window: Cell<Option<Weak<WindowData>>>,
+    handle: Cell<*mut GtkWidget>,
+    visibility: Cell<Visibility>,
+    text: Cell<String>,
+    resizable: Cell<bool>,
+}
+
+impl WindowBackend {
+    fn delete_handle(&self) {
+        if !self.handle.get().is_null() {
+            unsafe { gtk_widget_destroy(self.handle.get()); }
+            self.handle.set(ptr::null_mut());
+        }
+    }
+
+    fn recreate_handle(&self) {
+        unsafe {
+            self.handle.set(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+            // TODO: have to set text, resizable, etc.
+        }
+    }
+
+    fn is_handle_created(&self) -> bool {
+        !self.handle.get().is_null()
+    }
+
+    fn handle(&self) -> *mut GtkWidget {
+        if !self.is_handle_created() {
+            self.recreate_handle();
+        }
+        self.handle.get()
+    }
 }
 
 impl GenericWindowBackend for WindowBackend {
     fn new() -> WindowBackend {
-        WindowBackend { tmp: 0 }
+        WindowBackend {
+            window: Cell::new(None),
+            handle: Cell::new(ptr::null_mut()),
+            visibility: Cell::new(Visibility::Gone),
+            text: Cell::new("".to_string()),
+            resizable: Cell::new(true),
+        }
+    }
+
+    fn set_window(&self, window: Weak<crate::WindowData>) {
+        self.window.set(Some(window));
+    }
+
+    fn set_text(&self, text: &str) {
+        self.text.set(text.to_owned());
+        if self.is_handle_created() {
+            unsafe {
+                let mut text_buf = SmallVec::<[u8; 128]>::new();
+                let c_text = str_to_c_vec(text, &mut text_buf);
+                gtk_window_set_title(self.handle.get() as *mut GtkWindow, c_text);
+            }
+        }
+    }
+
+    fn visibility(&self) -> Visibility {
+        self.visibility.get()
+    }
+
+    fn set_visibility(&self, visibility: Visibility) {
+        self.visibility.set(visibility);
+        if self.visibility.get() == Visibility::Visible {
+            // TODO: this isn't how I did it in D
+            unsafe { gtk_widget_show(self.handle()); }
+        } else {
+            if self.is_handle_created() {
+                unsafe { gtk_widget_hide(self.handle.get()); }
+            }
+        }
+    }
+
+    fn set_location(&self, location: &Point2<f64>) {
+        todo!()
+    }
+
+    fn resizable(&self) -> bool {
+        self.resizable.get()
+    }
+
+    fn set_resizable(&self, resizable: bool) {
+        self.resizable.set(resizable);
+        if self.is_handle_created() {
+            unsafe {
+                gtk_window_set_resizable(
+                    self.handle.get() as *mut GtkWindow,
+                    resizable as gboolean);
+            }
+        }
     }
 }
 
