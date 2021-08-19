d9a289c64d53486125f5cdcbf82150185e1c238c
Jordan Miner <jminer7@gmail.com>
2021-08-19 01:46:55 -0500

    Add MPL license file and headers

--- src/cocoa_backend/mod.rs
+++ src/cocoa_backend/mod.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
 
 // include a very basic nib that loads the minimum possible
 // Use NSNib initWithNibData:bundle: with a nib that has been included in the library
